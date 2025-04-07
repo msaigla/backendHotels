291c9faa5940315b414411f4c94dcac95255d02d
Saigla <msaigla@ya.ru>
2025-04-07 16:35:47 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import uvicorn
+from fastapi import FastAPI, Query, Body
+
+app = FastAPI(debug=True)
+
+
+tables = [
+    {"id": 1, "title": "Table 1", "description": "столик у окна"},
+    {"id": 2, "title": "Table 2", "description": "столик у двери"},
+    {"id": 3, "title": "Table 3", "description": "столик у туалета"},
+]
+
+
+@app.get("/tables")
+def get_tables():
+    return tables
+
+
+@app.delete("/tables/{table_id}")
+def delete_table(table_id: int):
+    global tables
+    tables = [table for table in tables if table["id"] != table_id]
+    return {"status": "OK"}
+
+
+@app.post("/tables")
+def create_table(
+        title: str = Body(embed=True, description="Table title"),
+):
+    global tables
+    tables.append({"id": tables[-1]["id"] + 1, "title": title})
+    return {"status": "OK"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
