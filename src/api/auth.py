10f158b9a6ebe8e6d776140e0308b7db83589e5d
Saigla <msaigla@ya.ru>
2025-04-12 16:51:17 +0300

    read cookies

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,54 +1,44 @@
-from datetime import datetime, timezone, timedelta
-from http import HTTPStatus
-
-from fastapi import APIRouter, HTTPException
-
-from passlib.context import CryptContext
-import jwt
+from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.repos.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode |= ({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-@router.post("/login")
-async def login(
-        data: UserRequestAdd
-):
-    # hashed_password = pwd_context.hash(data.password)
-    # new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(email=data.email)
-        if not user:
-            raise HTTPException(status_code=401, detail="Пользователя не существует!")
-        access_token = create_access_token({"user_id": user.id})
-        return {"access_token": access_token}
-
 
 @router.post("/register")
-async def register(
+async def register_user(
         data: UserRequestAdd
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
     return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestAdd,
+        response: Response,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователя не существует!")
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
+
+
+@router.post("/only_auth")
+async def only_auth(
+        request: Request,
+):
+    access_token = request.cookies.get("access_token") or None
+    return {"access_token": access_token}
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 import sys
 from pathlib import Path
 
+from fastapi.openapi.docs import get_swagger_ui_html
+
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as hotels_router
@@ -13,6 +15,17 @@ app = FastAPI(debug=True)
 
 app.include_router(auth_router)
 app.include_router(hotels_router)
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,  # type: ignore
+        title=app.title + " - Swagger UI",  # type: ignore
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,  # type: ignore
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
