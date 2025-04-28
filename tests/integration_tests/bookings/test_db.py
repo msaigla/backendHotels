44f77660cb1b423eaad23ca5fc1b1a1a7387dd1c
Saigla <msaigla@ya.ru>
2025-04-28 14:57:30 +0300

    Добавил тестирование CRUD для бронирований

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -14,11 +14,31 @@ async def test_booking_crud(db):
         date_to=date(year=2025, month=5, day=20),
         price=100
     )
-    booking_data = await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
+
     # чтение брони
-    await db.bookings.get_one_or_none(id=booking_data.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
+
     # обновить бронь
-    await db.bookings.edit(booking_data)
+    update_date = date(year=2025, month=5, day=30)
+    update_booking_data = BookingAdd(
+        room_id=user_id,
+        user_id=room_id,
+        date_from=date(year=2025, month=5, day=10),
+        date_to=update_date,
+        price=100
+    )
+    await db.bookings.edit(update_booking_data, id=booking.id)
+    update_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert update_booking
+    assert update_booking.id == new_booking.id
+    assert update_booking.date_to == update_date
+
     # удалить бронь
-    await db.bookings.delete(id=booking_data.id)
-    await db.commit()
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
